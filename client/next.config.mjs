a60ad7cb78ef48c0782d1b37246739b8f3b09cb2
poojaraut2618 <poojaraut2618@gmail.com>
2025-09-27 17:40:48 +0530

    update next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: "export",
-    reactStrictMode: true,
-  swcMinify: true,
-  output: undefined,
+    
   images: {
     unoptimized: true
   },
